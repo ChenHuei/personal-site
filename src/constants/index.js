42c1526eee6fdc744a86f90519e026e09f0e1e92
LeoChen <leo_chen@mayohr.com>
2023-08-10 15:13:03 +0800

    update info

--- src/constants/index.js
+++ src/constants/index.js
@@ -18,7 +18,7 @@ export const ABOUT_INFORMATION = [
     name: '陳振輝',
     jobs: '前端工程師 (Front-End Engineer)',
     description:
-      '擁有五年前端開發經驗，熟悉 Typescript、React、Vue.js。追求簡潔易讀的程式碼，遵守 DRY 的開發概念建立易讀、高擴充性的 Component。重視團隊的合作與溝通，正在尋找一個能夠讓我穩定發揮並且不斷成長的環境。以下將更詳細說明過往經歷，非常感謝您抽空觀看，請多多指教，謝謝！',
+      '擁有五年前端開發經驗，熟悉 Typescript、React、Vue.js。追求簡潔易讀的程式碼，遵守 DRY 的開發概念建立易讀、高擴充性的 Component。重視團隊的合作與溝通，正在尋找一個能夠讓我穩定發揮並不斷成長的環境。以下將更詳細說明過往經歷，非常感謝您抽空觀看，請多多指教，謝謝！',
     phone: '0986-776-379',
     email: 'aaa830714@gmail.com',
   },
@@ -31,11 +31,11 @@ export const RESUME_INFORMATION = [{
     list: [
       {
         title: 'Apollo Asia',
-        description: '優化既有產品：增加網站 CSP 設定、統一專案的時區設定、修復網站長期以來久置會白頁的情況',
+        description: '優化既有產品，包括增加網站 CSP 設定、統一專案的時區設定、修復網站長期以來久置會白頁的情況',
       },
       {
         title: 'Apollo Asia - 越南',
-        description: '在既有的人資系統中 (React) 增加越南地區的功能和流程',
+        description: '在既有的人資系統中 (React) 增加越南地區的功能和情境',
       },
     ],
   },
