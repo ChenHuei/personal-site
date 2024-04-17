036fd08a01ea487db32324792e41f5abb4e0084e
LeoChen <leo_chen@mayohr.com>
2024-04-17 17:09:51 +0800

    update constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -18,7 +18,7 @@ export const ABOUT_INFORMATION = [
     name: '陳振輝',
     jobs: '資深前端工程師 (Senior Front-End Engineer)',
     description:
-      '擁有五年前端開發經驗，熟悉 Typescript、React、Vue.js。追求簡潔易讀的程式碼，遵守 DRY 的開發概念建立易讀、高擴充性的 Component。重視團隊的合作與溝通，正在尋找一個能夠讓我穩定發揮並不斷成長的環境。以下將更詳細說明過往經歷，非常感謝您抽空觀看，請多多指教，謝謝！',
+      '擁有五年前端開發經驗，熟悉 Typescript、React、Vue.js。追求簡潔易讀的程式碼，遵守 DRY 的開發概念建立易讀、高擴充性的 Component。重視團隊的合作與溝通，正在尋找一個能夠讓我穩定發揮並且不斷成長的環境。以下將更詳細說明過往經歷，非常感謝您抽空觀看，請多多指教，謝謝！',
     phone: '0986-776-379',
     email: 'aaa830714@gmail.com',
   },
@@ -26,18 +26,21 @@ export const ABOUT_INFORMATION = [
 
 export const RESUME_INFORMATION = [
   {
-    company: 'MAYO 鼎恒數位科技',
+    company: '鼎恒數位科技 (MAYO)',
     jobs: '資深前端工程師',
     year: '2022/06 ~ now',
     list: [
       {
-        title: 'Apollo Asia',
-        description:
-          '優化既有產品，包括增加網站 CSP 設定、統一專案的時區設定、修復網站長期以來久置會白頁的情況',
+        title: '優化產品',
+        description: '加入網站的 CSP 設定、安排專案的 React 升級計畫...',
       },
       {
-        title: 'Apollo Asia - 越南',
-        description: '在既有的人資系統中 (React) 增加越南地區的功能和情境',
+        title: '跨國、開發團隊',
+        description: '統一專案的時區設定，並依不同國家實作符合該地區法規及文化的功能',
+      },
+      {
+        title: '面試',
+        description: '參與 20 逾人的前端面試，並制定面試規範',
       },
     ],
   },
@@ -47,18 +50,22 @@ export const RESUME_INFORMATION = [
     year: '2021/07 ~ 2022/06',
     list: [
       {
-        title: '無人商店購物網站',
+        title: '技術選型',
         description:
-          '使用 Next.js 搭配 redux saga and ionic framework 實作仿 APP 網站 (WebView)，以及負責串接第三方金流、調整 react-native 相關功能',
-      },
-      {
-        title: 'Dashboard Monitor',
-        description: '使用 Gatsby 搭配 react query 建構後台',
+          '基於仿 APP 的產品性質，提出 ionic 取代 antd 成為新的 UI 框架提升使用者體驗，並成功落實於公司',
       },
       {
         title: 'Mentor',
         description:
-          '日常除了擔任另外三位 front-end 的 mentor 之外，同時也負責規劃和評估其他 junior 的 React 相關學習狀況',
+          '擔任三位 front-end engineer 的 mentor，同時也負責規劃和評估其他 junior 的 React 學習狀況',
+      },
+      {
+        title: '技術分享',
+        description: '分享 Next.js 的特點(v12)、Testing 的概念與實作，以及 airbnb 的 style guide',
+      },
+      {
+        title: '無人商店購物網站',
+        description: '使用 Next.js 建立專案、負責串接第三方金流，以及調整 react-native',
       },
     ],
   },
@@ -68,16 +75,8 @@ export const RESUME_INFORMATION = [
     year: '2019/09 ~ 2021/07',
     list: [
       {
-        title: '豪華郵輪集團 (Resorts World Genting)',
-        description: '使用 Nuxt.js 搭配 google sheet API 管理多國語系和 SSR',
-      },
-      {
-        title: '台塑電子商務平台',
-        description: '串接台塑 API 建構 RWD 線上交易平台',
-      },
-      {
-        title: '安麗簡訊平台 & 直播平台',
-        description: '使用 Typescript (vue-property-decorator) 建構前後台系統',
+        title: '豪華郵輪、台塑電子商務、安麗簡訊...',
+        description: '使用 Nuxt.js 以及 Typescript (vue-property-decorator) 建立專案',
       },
     ],
   },
@@ -87,16 +86,8 @@ export const RESUME_INFORMATION = [
     year: '2018/07 ~ 2019/05',
     list: [
       {
-        title: '漢堡王點餐系統',
-        description: '串接金流 API，開發線上點餐平台',
-      },
-      {
-        title: '發票管理平台',
-        description: '與行政人員討論需求、規劃流程，並串接藍新金流 API 開發電子發票平台',
-      },
-      {
-        title: '特店平台',
-        description: '沿用既有技術 jQuery 進行維運和開發',
+        title: '整合行動支付、漢堡王點餐系統、發票管理平台...',
+        description: '使用 Vue.js 建立專案，並串接各種電子支付和發票(藍新)的 API',
       },
     ],
   },
@@ -108,8 +99,7 @@ export const RESUME_INFORMATION = [
       {
         title: '校系影音分享 (Link)',
         description: '分享大學所學和課外活動的經驗及反思',
-        link:
-          'https://ioh.tw/talks/%E6%B7%A1%E6%B1%9F%E8%B3%87%E7%AE%A1%E7%B3%BB-%E9%99%B3%E6%8C%AF%E8%BC%9D-zhen-huei-chen-tw-study-tku-bde',
+        link: 'https://ioh.tw/talks/%E6%B7%A1%E6%B1%9F%E8%B3%87%E7%AE%A1%E7%B3%BB-%E9%99%B3%E6%8C%AF%E8%BC%9D-zhen-huei-chen-tw-study-tku-bde',
       },
       {
         title: '全台校園演講',
@@ -152,25 +142,18 @@ export const WORKS_INFORMATION = [
     description: '記帳網站',
     logo: require('../assets/works/huei-bei-money.png'),
     view: 'https://huei-bei-money.vercel.app',
-    skills: [
-      'vite with react-ts',
-      'PWA',
-      'Firebase Database & Authentication',
-      'Material UI & React Hook Form',
-      'GitHub Actions',
-    ],
+    skills: ['vite', 'PWA', 'Firebase', 'React Hook Form'],
   },
   {
     name: 'Huei & Bei Wedding',
     description: '婚禮網站',
     logo: require('../assets/works/huei-bei-wedding.png'),
-    view: 'https://chenhuei.github.io/huei-bei-wedding/',
+    view: 'https://chenhuei.github.io/huei-bei-wedding',
     skills: [
-      '@nuxtjs/pwa',
-      'Lighthouse & PageSpeed 評分 90+',
-      'GSAP & ScrollMagic',
-      'LINE Bot API & server-sent event (製作彈幕特效)',
-      'AWS amplify & route 53',
+      'PWA',
+      'performance scoring 90+',
+      'animation (GSAP, ScrollMagic)',
+      '彈幕 (LINE Bot, server-sent event)',
     ],
   },
   {
@@ -178,15 +161,15 @@ export const WORKS_INFORMATION = [
     description: '郵輪航線訂票平台系統 (前後台)',
     logo: require('../assets/works/genting.png'),
     view: 'http://genting-cruise-www-sit.cloud-softwares.com/enUS',
-    skills: ['Nuxt.js', 'google sheet API', 'WebView (RWD)'],
+    skills: ['Nuxt.js', 'RWD', 'WebView'],
   },
-  {
-    name: '台塑電子商務平台',
-    description: '線上交易平台',
-    logo: require('../assets/works/plastic.png'),
-    view: 'https://formosa-pepp-web-sit.cloud-interactive.com/login',
-    skills: ['Nuxt.js', '與台塑後端討論及介接 API', 'google sheet API', 'RWD'],
-  },
+  // {
+  //   name: '台塑電子商務平台',
+  //   description: '線上交易平台',
+  //   logo: require('../assets/works/plastic.png'),
+  //   view: 'https://formosa-pepp-web-sit.cloud-interactive.com/login',
+  //   skills: ['Nuxt.js', '與台塑後端討論及介接 API', 'google sheet API', 'RWD'],
+  // },
   {
     name: 'Scan2Pay',
     description: '串接線上支付方式',
